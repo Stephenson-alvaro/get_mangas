ffe6b1239c981abfdbf09377a9c27dba0fe8e89d
Stephenson-alvaro <novanime.data.github@gmail.com>
2025-04-25 17:05:49 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,9 +4,10 @@ from bs4 import BeautifulSoup
 
 async def scrape_page(url: str):
     async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        await page.goto(url)
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page(user_agent=("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"))
+        await page.goto(url, timeout=60000)
+        await page.wait_for_timeout(5000)  # attendre 5 secondes
         await page.wait_for_load_state("networkidle")
         content = await page.content()
         await browser.close()
